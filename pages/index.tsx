2afc63e8d757ac426b07b79d89e650e444dc8206
Monika Bugajewska <bugajewska.m@gmail.com>
2023-03-11 14:29:52 +0100

    Add filterData

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,63 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 import { useState } from "react";
 import { getEnvironmentData } from "worker_threads";
 import { getData } from "../data";
 import styles from "../styles/Home.module.css";
 import { Item } from "../interface";
-import { ListItem } from "@mui/material";
+import { ListItem, useScrollTrigger } from "@mui/material";
+import Movies from "./components/Movies";
+import Filtering from "./components/Filtering";
 
 const Home: NextPage = () => {
-  const [data, setData] = useState<Item[]| []>([]);
+  type Category = "Movie" | "TV Series" | "";
+  const [data, setData] = useState<Item[] | []>([]);
+
+  const [category, setCategory] = useState<Category>("");
   useEffect(() => {
-    getData().then(result => setData(result));
+    getData().then((result) => setData(result));
   }, []);
 
-
-console.log(data)
-  return (
-    <div className="container">
-{data?.map((move: Item )=> 
-    <div className="move" key={move.title}>{move.title}
-    <Image src={`/${move.thumbnail.regular.large}`} alt={move.title}
-     width="200px"
-     height="200px"/>
-    </div>
-    )
+  const handleMovie = ()=>{
+    
+    setCategory('Movie')
   }
-  </div>
+  const handleSeries = ()=>{
+    setCategory('TV Series')
+  }
+  const handleData = ()=>{
+    setCategory('')
+  }
+  const filterData  = useMemo(() => {
+    if (data !== null) {
+      if (category === "Movie"){
+        return data.filter((item) => item.category === "Movie");
+      }
+      if (category === 'TV Series'){
+        return data.filter((item)=> item.category === "TV Series")
+      }
+      if(category ===''){
+        return data
+      }
+      return;
+    }
+  }, [category])
+  console.log(filterData)
+  console.log(category)
+  console.log(data);
+  return (
+    <>
+      <div className="webside">
+        <div>
+          <Filtering handleMovie={handleMovie} handleSeries={handleSeries} handleData={handleData}/>
+        </div>
+        <div>
+          <Movies moviesOrSeries={filterData} />
+        </div>
+      </div>
+    </>
   );
 };
 
